84b6517565781fc61951f2458d3af540ef68fed5
23rd002 <23rd002@ms.dendai.ac.jp>
2025-10-27 18:43:03 +0900

    研究室概要・文章

--- Frontend/src/routes/about.tsx
+++ Frontend/src/routes/about.tsx
@@ -1,8 +1,19 @@
 export const About: React.FC = () => {
   return (
     <div className="container py-12">
-      <h1 className="text-2xl font-semibold">研究室概要</h1>
-      <p className="mt-4 text-muted-foreground">このページは準備中です。</p>
+      <h1 className="text-4xl font-bold tracking-tight sm:text-5xl">
+              Smart ICT Solutions Laboratory
+      </h1>
+      <h1 className="text-2xl font-semibold">スマートICTソリューション研究室（設立：2023年）</h1>
+      <p className="mt-4 text-muted-foreground">私たちは、最新のICT（情報通信技術）を活用して、より賢く便利な社会システムを実現する方法を探求する研究室です。
+                IoTやAI、デジタルツイン、メタバースなどの先端技術を組み合わせ、社会や産業で役立つシステムづくりに取り組んでいます。
+
+                研究を通して、単に技術を学ぶだけでなく、自分で考え、行動し、課題を解決することを目指し、研究と実装を繰り返しながら社会に貢献できる人材の育成に力を入れています。
+
+                また、研究だけでなく、人とのつながりやチームワークも大切にしています。
+                学生同士の仲がとても良く、夏合宿や懇親会（クリスマス会・タコパなど）を年に数回開催しています。
+                明るくアットホームな雰囲気の中で、楽しみながら研究に取り組める環境です
+      </p>
     </div>
   )
 }
